1ab4f27bd24d180d3f87f1511daac1fc73487d4f
Mandemik <harewind@gmail.com>
2022-06-19 16:28:48 +0200

    InitCommit

---
+++ Source/MagBuildGame/Public/BaseAttributeSet.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "BaseAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+/**
+ * 
+ */
+UCLASS()
+class MAGBUILDGAME_API UBaseAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UBaseAttributeSet();
+
+	//Attributes for health
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "BaseAttribute")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Health);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttribute")
+		FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxHealth);
+
+
+	//Attributes for Mana
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttribute")
+		FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Mana);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttribute")
+		FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxMana);
+
+	//Attributes for Stamina
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttribute")
+		FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Stamina);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseAttribute")
+		FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxStamina);
+
+	void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+
+};
---
+++ Source/MagBuildGame/Public/BaseCharacter.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//#include "AbilitySystemComponent.h"
+//#include "AbilitySystemInterface.h"
+//#include "Abilities/GameplayAbility.h"
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "Abilities/GameplayAbility.h"
+#include "BaseCharacter.generated.h"
+
+class UBaseAttributeSet;
+
+UCLASS()
+class MAGBUILDGAME_API ABaseCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "BaseCharacter")
+		UAbilitySystemComponent* AbilitySystemComp;
+
+	UFUNCTION(BlueprintCallable, Category = "BaseCharacter")
+		void InitializeAbility(TSubclassOf<UGameplayAbility> AbilityToGet, int32 AbilityLevel);
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite,Category = "BaseCharacter")
+		const class UBaseAttributeSet* BaseAttributeSetComp;
+
+	UFUNCTION(BlueprintPure,Category = "Base Character")
+		void GetHealthValues(float& Health, float& MaxHealth);
+
+	UFUNCTION(BlueprintPure, Category = "Base Character")
+		void GetManaValues(float& Mana, float& MaxMana);
+
+	UFUNCTION(BlueprintPure, Category = "Base Character")
+		void GetStaminaValues(float& Stamina, float& MaxStamina);
+
+	void OnHealthChangedNative(const FOnAttributeChangeData& Data);
+	void OnManaChangedNative(const FOnAttributeChangeData& Data);
+	void OnStaminaChangedNative(const FOnAttributeChangeData& Data);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "BaseCharacter")
+		void OnHealthChanged(float OldValue, float NewValue);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "BaseCharacter")
+		void OnManaChanged(float OldValue, float NewValue);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "BaseCharacter")
+		void OnStaminaChanged(float OldValue, float NewValue);
+
+
+};
