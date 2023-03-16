b5afe8763292ca88e50df94a7400eb22b468c54a
Mandemik <harewind@gmail.com>
2023-03-16 09:45:40 +0100

    Added new GameplayAbility Class + added gameplay task plugin

---
+++ Source/MagBuildGame/Public/MagGameplayAbility.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayTaskOwnerInterface.h"
+#include "MagGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGBUILDGAME_API UMagGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
